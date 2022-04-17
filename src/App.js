2b0aa01fa65302ee1ad372a8e39992685cf7669d
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2022-04-17 23:49:30 +0100

    Created categoriesOptions.js and converted hard coded list to dynamic list in App.js

--- src/App.js
+++ src/App.js
@@ -1,41 +1,26 @@
+import { categories } from './options/categoriesOptions';
+
 const App = () => {
   return (
     <div className='categories-container'>
-      <div className='category-container'>
-        {/*<img src="" alt=""/>*/}
-        <div className='category-body-container'>
-          <h2>Hats</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className='category-container'>
-        {/*<img src="" alt=""/>*/}
-        <div className='category-body-container'>
-          <h2>Jackets</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className='category-container'>
-        {/*<img src="" alt=""/>*/}
-        <div className='category-body-container'>
-          <h2>Sneakers</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className='category-container'>
-        {/*<img src="" alt=""/>*/}
-        <div className='category-body-container'>
-          <h2>Womens</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className='category-container'>
-        {/*<img src="" alt=""/>*/}
-        <div className='category-body-container'>
-          <h2>Mens</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
+      {
+        categories.map(({id, title}) => (
+          <div
+            key={id}
+            className='category-container'
+          >
+            <div
+              className='background-image'
+            />
+            <div
+              className='category-body-container'
+            >
+              <h2>{title}</h2>
+              <p>Shop Now</p>
+            </div>
+          </div>
+        ))
+      }
     </div>
   );
 }
