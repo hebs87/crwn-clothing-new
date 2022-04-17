f9d401f4e9cf8cc31fd9be28d7670367dee43395
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2022-04-18 00:20:06 +0100

    Split categories container and item into separate components

--- src/App.js
+++ src/App.js
@@ -1,31 +1,10 @@
-import { categories } from './options/categoriesOptions';
-import './categories.styles.scss';
+import CategoriesDirectory from './components/CategoriesDirectory/CategoriesDirectory';
 
 const App = () => {
   return (
-    <div className='categories-container'>
-      {
-        categories.map(({id, title, imageUrl}) => (
-          <div
-            key={id}
-            className='category-container'
-          >
-            <div
-              className='background-image'
-              style={{
-                backgroundImage: `url(${imageUrl})`
-              }}
-            />
-            <div
-              className='category-body-container'
-            >
-              <h2>{title}</h2>
-              <p>Shop Now</p>
-            </div>
-          </div>
-        ))
-      }
-    </div>
+    <>
+      <CategoriesDirectory/>
+    </>
   );
 }
 
---
+++ src/components/CategoriesDirectory/CategoriesDirectory.jsx
@@ -0,0 +1,21 @@
+import { categories } from '../../options/categoriesOptions';
+import CategoryItem from '../CategoryItem/CategoryItem';
+import './CategoriesDirectory.scss';
+
+const CategoriesDirectory = () => {
+  return (
+    <div className='categories-directory'>
+      {
+        categories.map(({id, title, imageUrl}) => (
+          <CategoryItem
+            key={id}
+            title={title}
+            imageUrl={imageUrl}
+          />
+        ))
+      }
+    </div>
+  );
+};
+
+export default CategoriesDirectory;
