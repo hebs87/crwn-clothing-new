b0eb8c9cf35cab836ec07935f852220c4081d553
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2022-04-18 11:18:27 +0100

    Rendered NavBar in App.js and updated Route structure to leverage nested routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import { Routes, Route } from 'react-router-dom';
+import NavBar from './components/NavBar/NavBar';
 import Home from './Pages/Home';
 
 const App = () => {
   return (
     <Routes>
-      <Route path='/' element={<Home/>} exact />
+      {/*
+        Outlet component in NavBar allows rendering content in nested Routes below it
+        index prop in Home Route allows rendering that component as index component
+      */}
+      <Route path='/' element={<NavBar/>}>
+        <Route index element={<Home/>} />
+      </Route>
     </Routes>
   );
 }
