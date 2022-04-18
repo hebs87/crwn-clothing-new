5e614d7e0c2523cd349c3bf6b0ddc3520b28f251
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2022-04-18 21:53:32 +0100

    Imported and rendered Login component in App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Routes, Route } from 'react-router-dom';
 import NavBar from './components/NavBar/NavBar';
+import Login from './Pages/Login/Login';
 import Home from './Pages/Home/Home';
 
 const App = () => {
@@ -10,6 +11,7 @@ const App = () => {
         index prop in Home Route allows rendering that component as index component
       */}
       <Route path='/' element={<NavBar/>}>
+        <Route path='login' element={<Login/>} />
         <Route index element={<Home/>} />
       </Route>
     </Routes>
