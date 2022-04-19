6d80dc2ef70bb7464968875d24217e2960a0aa00
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2022-04-20 00:15:34 +0100

    Created reusable FormInput component with custom styles

---
+++ src/components/FormInput/FormInput.jsx
@@ -0,0 +1,24 @@
+import './FormInput.styles.scss';
+
+const FormInput = ({label, ...otherProps}) => {
+  return (
+    <div className='group'>
+      <input
+        className='form-input'
+        {...otherProps}
+      />
+      {
+        label && otherProps.id && (
+          <label
+            htmlFor={otherProps.id}
+            className={`${otherProps.value.length ? 'shrink' : ''} form-input-label`}
+          >
+            {label}
+          </label>
+        )
+      }
+    </div>
+  );
+};
+
+export default FormInput;
