4908d732f81e2f768ecd35c6f2005e4abf3b36eb
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2022-04-19 22:52:07 +0100

    Implemented initial signInWithPopup functionality in Login.jsx

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,7 +1,19 @@
+import { signInWithGooglePopup } from '../../utils/firebase/firebase.utils';
+
 const Login = () => {
+  const logGoogleUser = async () => {
+    const response = await signInWithGooglePopup();
+    console.log(response);
+  };
+
   return (
     <div>
       <h1>Login Page</h1>
+      <button
+        onClick={logGoogleUser}
+      >
+        Sign in with Google Popup
+      </button>
     </div>
   );
 };
