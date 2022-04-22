94329b4441945853c6f4db4676ed2b37588103ca
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2022-04-22 22:57:52 +0100

    Rendered LoginForm component in Authentication.jsx

--- src/Pages/Authentication/Authentication.jsx
+++ src/Pages/Authentication/Authentication.jsx
@@ -1,25 +1,12 @@
-import {
-  signInWithGooglePopup,
-  createUserDocumentFromAuth
-} from '../../utils/firebase/firebase.utils';
+import LoginForm from '../../components/LoginForm/LoginForm';
 import RegistrationForm from '../../components/RegistrationForm/RegistrationForm';
 
 const Authentication = () => {
-  const logGoogleUser = async () => {
-    // Get the user from the popup
-    const {user} = await signInWithGooglePopup();
-    // Get existing userDocRef from Firestore, or create one if it does not exist
-    const userDocRef = await createUserDocumentFromAuth(user);
-  };
 
   return (
     <div>
       <h1>Login Page</h1>
-      <button
-        onClick={logGoogleUser}
-      >
-        Sign in with Google Popup
-      </button>
+      <LoginForm/>
       <RegistrationForm/>
     </div>
   );
