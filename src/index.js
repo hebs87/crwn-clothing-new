90ac8a4f16df8b0b4f73f4af5f5616e4a2e835b3
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2022-04-22 23:35:05 +0100

    Rendered UserProvider in index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {BrowserRouter as Router} from 'react-router-dom';
-import './index.scss';
 import App from './App';
+import { UserProvider } from './contexts/User.context';
 import reportWebVitals from './reportWebVitals';
+import './index.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router>
-      <App />
+      <UserProvider>
+        <App />
+      </UserProvider>
     </Router>
   </React.StrictMode>
 );
