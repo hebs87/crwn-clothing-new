06b8c2a72ddab24bff23458b8875ef6648cc647e
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2022-04-24 12:36:38 +0100

    Added functionality to redirect user to home page if logged in in Authentication.jsx

--- src/Pages/Authentication/Authentication.jsx
+++ src/Pages/Authentication/Authentication.jsx
@@ -1,8 +1,16 @@
+import { useContext, useEffect } from 'react';
+import { UserContext } from '../../contexts/User.context';
 import LoginForm from '../../components/LoginForm/LoginForm';
 import RegistrationForm from '../../components/RegistrationForm/RegistrationForm';
 import './Authentication.styles.scss';
 
 const Authentication = () => {
+  const {currentUser} = useContext(UserContext);
+
+  useEffect(() => {
+    // Redirect to home page if user is logged in
+    if (currentUser) window.location.href = '/';
+  });
 
   return (
     <div
